b39cc58189d9c2bb56fffb65470ba08cc58b6c4e
gisempire <gisempire@foxmail.com>
2019-08-09 19:52:38 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,7 +1,27 @@
-
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QApplication)
+from PyQt5.QtWidgets import QApplication,QWidget
+
+
+if __name__=='__main__':
+    
+    app=QApplication(sys.argv)
+
+    try:
+        if len(sys.argv) < 2:
+            raise ValueError
+        else:
+            title = " ".join(sys.argv[1:])
+    except ValueError:
+        title = "我的界面"
+
+    w=QWidget()
+    w.resize(500,300)
+    w.move(1800,100)
+    w.setWindowTitle(title)
+    w.show()
+
+    sys.exit(app.exec_())
+
 
 
 
